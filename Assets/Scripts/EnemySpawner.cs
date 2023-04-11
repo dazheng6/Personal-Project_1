1c737a80c9c6784482b9535e281e5e17a8f814e3
dazheng6 <121971803+dazheng6@users.noreply.github.com>
2023-04-11 14:31:06 -0700

    Made a Title Screen, and a Start Button

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -8,15 +9,35 @@ public class EnemySpawner : MonoBehaviour
     public Material[] materials;
     public float spawnRadius = 20f;
     public float timeToSpawn = 1.5f;
+    public Button startButton;
+    public GameObject playerPrefab;
+    public GameObject titleScreen;
+    public Button restartButton;
+
+    private bool gameStarted = false;
+    private bool gameOver = false;
 
     private void Start()
     {
-        StartCoroutine(SpawnEnemies());
+        startButton.onClick.AddListener(StartGame);
+        restartButton.gameObject.SetActive(false);
+    }
+
+    private void StartGame()
+    {
+        if (!gameStarted)
+        {
+            gameStarted = true;
+            startButton.gameObject.SetActive(false);
+            titleScreen.SetActive(false); // Hide the title screen
+            SpawnPlayer();
+            StartCoroutine(SpawnEnemies());
+        }
     }
 
     private IEnumerator SpawnEnemies()
     {
-        while (true)
+        while (gameStarted && !gameOver)
         {
             Vector3 randomPosition = Random.onUnitSphere * spawnRadius + transform.position;
             randomPosition.y = 1.73f;
@@ -28,5 +49,39 @@ public class EnemySpawner : MonoBehaviour
             }
             yield return new WaitForSeconds(timeToSpawn);
         }
+    }
+
+    private void SpawnPlayer()
+    {
+        Instantiate(playerPrefab, new Vector3(0, 1.73f, 0), Quaternion.identity);
+    }
+
+    public void RestartGame()
+    {
+        gameOver = false;
+        gameStarted = false;
+        restartButton.gameObject.SetActive(false);
+        startButton.gameObject.SetActive(true);
+        titleScreen.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        gameOver = true;
+        StopCoroutine(SpawnEnemies());
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemy in enemies)
+        {
+            Destroy(enemy);
+        }
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            Destroy(player);
+        }
+
+        restartButton.gameObject.SetActive(true);
     }
 }
